d8160f83b056c41da369b302f273ec62ebc7ebaa
Chisato-Andy <aceofspyair533@gmail.com>
2025-01-12 16:50:46 +0900

    メモ画面追加

---
+++ shared_notes_tab.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+def create_shared_notes_tab(notebook):
+    tab = ttk.Frame(notebook)  # 新しいタブ作成
+    notebook.add(tab, text="shared_notes")  # "メモ" タブを追加
+
+    tab.columnconfigure(0, weight=1)
+    tab.columnconfigure(1, weight=1)
+
+    # memo書く
+    label = tk.Label(tab, text="BBB", font=("Helvetica", 14), anchor="w")  # 文字列"AAA"を表示
+    label.grid(row=0, column=0, padx=10, pady=10, sticky="w")  # gridで配置
+
+    return tab
